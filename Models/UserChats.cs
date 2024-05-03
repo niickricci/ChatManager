aa8400a30f59a8f2c5a4aae0823f56077fc95ef8
Ｎｉｃｋ  Ｒｉｃｃｉ <95265278+niickricci@users.noreply.github.com>
2024-05-03 03:04:50 -0400

    Create UserChats.cs
    -Created a new model (UserChats) to store messages between users

---
+++ Models/UserChats.cs
@@ -0,0 +1,13 @@
+﻿using ChatManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Services.Description;
+
+public class UserChats 
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int FriendId { get; set; }
+    public string Message { get; set; }
+    public DateTime Date { get; set; }
+}
